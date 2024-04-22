cef8f7274f3a9f61d07b05c2dd570127dcc7b821
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-04-22 15:19:44 +0800

    .

--- week_9/theorem_value.py
+++ week_9/theorem_value.py
@@ -3,14 +3,9 @@ import math
 def f(N, L, T, rho):
     return N/(2*L)*math.sqrt(T/rho)
 
-# N = float(input("N: "))
-# L = float(input("L(m): "))
-# T = float(input("T(N): "))
-# rho = float(input("rho(kg/m^3): "))
-# g = 9.8
 
-if __name__ == "__main__":
 
+def main_1():
     T_list = [0.00055, 0.00098, 0.00191, 0.00350, 0.00578, 0.00936]
     for i in T_list:
         print(f"\\rho = {i}")
@@ -18,3 +13,15 @@ if __name__ == "__main__":
             f1 = f(1, 0.5, j*1.96, i)
             print(f"T = {j*1.96} kg, f = {f1} Hz")
         print
+
+def main_2():
+    N = float(input("N: "))
+    L = float(input("L(m): "))
+    T = float(input("T(N): "))
+    rho = float(input("rho(kg/m): "))
+    g = 9.8
+    f1 = f(N, L, T, rho)
+    print(f"f = {f1} Hz")
+
+if __name__ == "__main__":
+    main_1()
