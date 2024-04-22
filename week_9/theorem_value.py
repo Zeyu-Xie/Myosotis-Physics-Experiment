a1f6fc93c8311c35899ae06912cea604bb868946
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-04-22 15:32:01 +0800

    .

--- week_9/theorem_value.py
+++ week_9/theorem_value.py
@@ -23,5 +23,11 @@ def main_2():
     f1 = f(N, L, T, rho)
     print(f"f = {f1} Hz")
 
+def main_3():
+    T_list = [0.00055, 0.00098, 0.00191, 0.00350, 0.00578, 0.00936]
+    for i in T_list:
+        # f1 = f(1, 0.5, 9.8, i)
+        print(f"理论 v = {math.sqrt(9.8/i)} m/s")
+
 if __name__ == "__main__":
-    main_1()
+    main_3()
